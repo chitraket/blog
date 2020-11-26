2af43d3181a61d1d4b4e716205100970ebe8c2a4
chit <chitraketsavani@gmail.com>
2020-11-26 12:07:21 +0530

    version 1.15

--- resources/views/user/error.blade.php
+++ resources/views/user/error.blade.php
@@ -12,7 +12,7 @@
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-10 col-lg-8">
                 <div class="title text-center">
-                <img class="img-fluid d-flex  mr-3" src="{{ url('images/error-not-found-page.png') }} " alt="error-not-found-page.png">
+                <img class="img-fluid d-flex  mr-3" src="{{ url('images/error-not-found-page.png') }}" alt="">
                 <h1 class="mb-10 m-2">{{__('error.errors')}} :(</h1>
                 </div>
             </div>
